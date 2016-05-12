3a767b3d4567d0294d6951d29fa110e34110bf83
FilipJoh <filip.johannesson@gmail.com>
2016-05-12 12:14:47 +0200

    some more editing with essential matrix

--- Test5point.m
+++ Test5point.m
@@ -17,9 +17,9 @@ imagesc(i1)
 figure;
 imagesc(i2)
 i1_gray = rgb2gray(i1);
-detected_pts_i1 = detectSURFFeatures(i1_gray,'NumOctaves',8);
+detected_pts_i1 = detectSURFFeatures(i1_gray,'NumOctaves',20);
 i2_gray = rgb2gray(i2);
-detected_pts_i2 = detectSURFFeatures(i2_gray,'NumOctaves',8);
+detected_pts_i2 = detectSURFFeatures(i2_gray,'NumOctaves',20);
 [snapshot_features_i1, i1_pts] = extractFeatures(i1_gray, detected_pts_i1);
 [snapshot_features_i2, i2_pts] = extractFeatures(i2_gray, detected_pts_i2);
 index_pairs = matchFeatures(snapshot_features_i1, snapshot_features_i2);
@@ -40,7 +40,7 @@ im2=i2;
 iterations=1000;
 bestE=[];
 %bestEInd=0.02;
-inlierDiscriminator=0.008;
+inlierDiscriminator=0.00008;
 bestIndices=[];
 comparator=zeros(1,size(input2all,2));
 inliermask=zeros(1,size(input2all,2));
@@ -53,35 +53,30 @@ for j=1:iterations
     input1=input1all(:,indices);
     input2=input2all(:,indices);
     E=Ematrix5pt(input1, input2);
-%     Evec=calibrated_fivepoint(input1(:,1:5), input2(:,1:5));
-%     if size(Evec,1)~=1
         for a=1:size(E,3)
-%             E=reshape(Evec(:,a),3,3);
-            Ecell{j,a}=E(:,:,a);
+            Ecell{j,a}=E(:,:,a)';
             %Test epipolar contraint
             for i=1:size(input2all,2)
-                comparator(i)=input2all(:,i)'*Ecell{j,a}*input1all(:,i);
-                comparator(i)=abs(comparator(i));
+                samp2=input2all(:,i)'*Ecell{j,a};
+                samp1=Ecell{j,a}*input1all(:,i);
+                comparator(i)=(input2all(:,i)'*Ecell{j,a}*input1all(:,i))/sqrt(samp2(1)^2+samp2(2)^2+samp1(1)^2+samp1(2)^2);
+                
+                
+                 comparator(i)=(comparator(i))^2;
+                
+                
                 inliermask(i)= comparator(i)<inlierDiscriminator;
-%                 inliercounter=length(find(inlierIndex));
             end
             inliers{j,a}=find(inliermask);
             inliercounter(j,a)=sum(inliermask);
-            %comparator=max(input2all'*E*input1all);
-%             if inliercounter>maxinliers;
-%                 bestE=E;
-%                 bestEInd=max(comparator);
-%                 bestIndices=find(inlierIndex);
-%             end
         end
-%     end
 end
 [bestRow,bestCol]=find(inliercounter==max(max(inliercounter)));
 bestE=Ecell{bestRow,bestCol};
 bestIndices=inliers{bestRow,bestCol};
-
-[F,inliersIndex]=estimateFundamentalMatrix(matched_i1_pts,matched_i2_pts);
-showMatchedFeatures(i1,i2,matched_i1_pts(inliersIndex),matched_i2_pts(inliersIndex),'Montage')
+% figure;
+% [F,inliersIndex]=estimateFundamentalMatrix(matched_i1_pts,matched_i2_pts);
+% showMatchedFeatures(i1,i2,matched_i1_pts(inliersIndex),matched_i2_pts(inliersIndex),'Montage')
 
 if isempty(bestE)==1
     %locs = matched_snapshot_pts_coord;
@@ -145,12 +140,12 @@ for i=1:4
     figure
     imagesc(im2);
     hold on
-    plot(uncal(1,:),uncal(2,:),'b*');
+    plot(uncal(1,bestIndices),uncal(2,bestIndices),'b*');
     plot(pProj2{i}(1,:),pProj2{i}(2,:),'go')
     hold off
-    for j=1:length(pProj2{i})
-        projerror{i}(j)=sqrt((pProj2{i}(1,j)-uncal(1,j)).^2+(pProj2{i}(2,j)-uncal(2,j)).^2);
-    end
+    %for j=1:length(pProj2{i})
+        %projerror{i}(j)=sqrt((pProj2{i}(1,j)-uncal(1,j)).^2+(pProj2{i}(2,j)-uncal(2,j)).^2);
+    %end
 end
 
 
